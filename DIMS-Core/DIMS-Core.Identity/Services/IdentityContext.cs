ed1e4b7f8f0587ca4dc6d5811e1c1b17a1396c52
Konstantin <33716293+PushMeNow@users.noreply.github.com>
2020-06-23 17:48:21 +0300

    Fix dependency error for identity

--- DIMS-Core/DIMS-Core.Identity/Services/IdentityContext.cs
+++ DIMS-Core/DIMS-Core.Identity/Services/IdentityContext.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DIMS_Core.Identity.Services
 {
     public class IdentityContext : DbContext
     {
+        public IdentityContext([NotNull] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected IdentityContext()
+        {
+        }
     }
 }
