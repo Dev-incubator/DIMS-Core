4218d2f83072f2501a529ef9497215562c431a58
darkpchela <darkpchela2@gmail.com>
2020-07-21 18:26:47 +0300

    Carcase

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Intefaces/IUserProfileManageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIMS_Core.BusinessLayer.Intefaces
+{
+    public interface IUserProfileManageService
+    {
+
+    }
+}
