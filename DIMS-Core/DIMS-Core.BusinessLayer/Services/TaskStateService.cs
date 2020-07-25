7a8a539abda8201cbf28685ff809beb05df4488c
darkpchela <darkpchela2@gmail.com>
2020-07-26 01:39:59 +0300

    TaskState Service Added

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Services/TaskStateService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.BusinessLayer.Models.BaseModels;
+using DIMS_Core.DataAccessLayer.Entities;
+using DIMS_Core.DataAccessLayer.Interfaces;
+
+namespace DIMS_Core.BusinessLayer.Services
+{
+    public class TaskStateService : GenericCRUDService<TaskState, TaskStateModel>, ITaskStateService
+    {
+        public TaskStateService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
+
+        private protected override IRepository<TaskState> BaseRepository => unitOfWork.TaskStateRepository;
+    }
+}
