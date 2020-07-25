25835e6d9e5b5b21c1bf4c45148493cf8c1c3b45
darkpchela <darkpchela2@gmail.com>
2020-07-26 02:06:34 +0300

    VTask Service Added

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/MappingProfiles/VTaskProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Models.BaseModels;
+using DIMS_Core.DataAccessLayer.Entities;
+
+namespace DIMS_Core.BusinessLayer.MappingProfiles
+{
+    public class VTaskProfile : Profile
+    {
+        public VTaskProfile()
+        {
+            CreateMap<VTask, VTaskModel>();
+        }
+    }
+}
