604e4d91d2093323c8dc4c246ec7560b309f05f1
darkpchela <darkpchela2@gmail.com>
2020-07-26 02:19:55 +0300

    VUserTaskService Added

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/MappingProfiles/VUserTaskProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Models.BaseModels;
+using DIMS_Core.DataAccessLayer.Entities;
+
+namespace DIMS_Core.BusinessLayer.MappingProfiles
+{
+    public class VUserTaskProfile : Profile
+    {
+        public VUserTaskProfile()
+        {
+            CreateMap<VUserTask, VUserTaskModel>().ReverseMap();
+        }
+    }
+}
