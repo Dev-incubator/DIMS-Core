a71f55fb0a2148e25db39e5a506fa1f7026aa6a3
darkpchela <darkpchela2@gmail.com>
2020-07-26 01:49:50 +0300

    TaskTrack Service Addedf

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/MappingProfiles/TaskTrackProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Models.BaseModels;
+using DIMS_Core.DataAccessLayer.Entities;
+
+namespace DIMS_Core.BusinessLayer.MappingProfiles
+{
+    public class TaskTrackProfile : Profile
+    {
+        public TaskTrackProfile()
+        {
+            CreateMap<TaskTrack, TaskTrackModel>().ReverseMap();
+        }
+    }
+}
