d3bcea4a51bd56c8dbc350b3c85ddf3e55a69187
darkpchela <darkpchela2@gmail.com>
2020-07-26 01:13:41 +0300

    UserProfile CRUD Service added

--- DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/IUserProfileService.cs
+++ DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/IUserProfileService.cs
@@ -1,15 +1,8 @@
 ﻿using DIMS_Core.BusinessLayer.Models.BaseModels;
-using System.Threading.Tasks;
 
 namespace DIMS_Core.BusinessLayer.Interfaces
 {
-    public interface IUserProfileService
+    public interface IUserProfileService : IGenericCRUDService<UserProfileModel>
     {
-        Task GetUserProfile(int userId);
-        Task DeleteUserProfile(int userProfileId);
-
-        Task UpdateUserProfile(UserProfileModel userModel);
-
-        Task CreateUserProfile(UserProfileModel userModel);
     }
 }
