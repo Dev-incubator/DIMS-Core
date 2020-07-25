b8fc5cecd2d24209b6b4b19bd8c3d1d0a095a358
darkpchela <darkpchela2@gmail.com>
2020-07-26 02:26:53 +0300

    VUserTrack Service Added

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Services/VUserTrackService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.BusinessLayer.Models.BaseModels;
+using DIMS_Core.DataAccessLayer.Entities;
+using DIMS_Core.DataAccessLayer.Interfaces;
+
+namespace DIMS_Core.BusinessLayer.Services
+{
+    public class VUserTrackService : GenericCRUDService<VUserTrack, VUserTrackModel>, IVUserTrackService
+    {
+        public VUserTrackService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
+
+        private protected override IRepository<VUserTrack> BaseRepository => unitOfWork.VUserTrackRepository;
+    }
+}
