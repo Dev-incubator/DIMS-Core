4d3f193a2861c05a00511ea8ce50c993f3a1303b
darkpchela <darkpchela2@gmail.com>
2020-07-26 01:26:45 +0300

    DirectionService added

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/MappingProfiles/DirectionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Models.BaseModels;
+using DIMS_Core.DataAccessLayer.Entities;
+
+namespace DIMS_Core.BusinessLayer.MappingProfiles
+{
+    public class DirectionProfile : Profile
+    {
+        public DirectionProfile()
+        {
+            CreateMap<Direction, DirectionModel>().ReverseMap();
+        }
+    }
+}
