8f0711dd99d993c6a988eb83c1a6aaeb9e0de88d
darkpchela <darkpchela2@gmail.com>
2020-07-27 18:23:01 +0300

    Testing implementions of Roles added

--- DIMS-Core/DIMS-Core/Program.cs
+++ DIMS-Core/DIMS-Core/Program.cs
@@ -2,14 +2,39 @@ using DIMS_Core.Logger.Extensions;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using DIMS_Core.Identity.Configs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using DIMS_Core.Identity.Entities;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace DIMS_Core
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<Role>>();
+                    await RoleInitializer.InitializeAsync(userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateHostBuilder(string[] args)
