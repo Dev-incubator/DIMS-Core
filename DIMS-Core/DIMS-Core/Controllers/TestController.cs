5441567cce67f455d381faecfada0b88a759d887
darkpchela <darkpchela2@gmail.com>
2020-07-27 17:17:55 +0300

    Added actions and links to TestView

--- DIMS-Core/DIMS-Core/Controllers/TestController.cs
+++ DIMS-Core/DIMS-Core/Controllers/TestController.cs
@@ -8,7 +8,38 @@ namespace DIMS_Core.Controllers
 {
     public class TestController : Controller
     {
+        //<a asp-action="Index">Action list</a>
+        //<a asp-action= "Members" > Members manage grid</a>
+        //<a asp-action= "MemberProgress" > MemberProgress manage grid</a>
+        //<a asp-action= "MembersTasks" > Memeber's Tasks manage grid</a>
+        //<a asp-action= "Tasks" > Tasks manage grid</a>
+        //<a asp-action= "TaskTracks" > TaskTracks manage grid</a>
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MembersManageGrid()
+        {
+            return View();
+        }
+
+        public IActionResult MemberProgressGrid()
+        {
+            return View();
+        }
+
+        public IActionResult MembersTasksManageGrid()
+        {
+            return View();
+        }
+
+        public IActionResult TasksManageGrid()
+        {
+            return View();
+        }
+
+        public IActionResult TaskTracksManageGrid()
         {
             return View();
         }
