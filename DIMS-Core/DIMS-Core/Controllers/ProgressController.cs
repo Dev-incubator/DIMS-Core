75b820c0e60765a5567bb42d5362d6085997a616
darkpchela <darkpchela2@gmail.com>
2020-07-31 22:01:07 +0300

    Progress Controller and View carcase made

---
+++ DIMS-Core/DIMS-Core/Controllers/ProgressController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIMS_Core.Controllers
+{
+    public class TrackManagerController : Controller
+    {
+        public IActionResult MembersTasksManageGrid()
+        {
+            return View();
+        }
+
+        public IActionResult TaskTracksManageGrid()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> EditNote()
+        {
+            return PartialView("EditNoteWindow");
+        }
+
+        public async Task<IActionResult> DeleteNote()
+        {
+            return View("TaskTracksManageGrid");
+        }
+
+        public async Task<IActionResult> SetSuccess()
+        {
+            return View("MembersTasksManageGrid");
+        }
+
+        public async Task<IActionResult> SetFail()
+        {
+            return View("MembersTasksManageGrid");
+        }
+    }
+}
