e20f02807effa594348bf8c1626eca49b1d76b28
darkpchela <darkpchela2@gmail.com>
2020-08-08 12:54:52 +0300

    Unnecessary methods at repository removed

--- DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IVUserProfileRepository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IVUserProfileRepository.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DIMS_Core.DataAccessLayer.Entities;
+﻿using DIMS_Core.DataAccessLayer.Entities;
 
 namespace DIMS_Core.DataAccessLayer.Interfaces
 {
     public interface IVUserProfileRepository : IRepository<VUserProfile>
     {
-        Task<VUserProfile> GetVUserProfileByEmail(string email);
-        IQueryable<VUserProfile> GetVUserProfilesByDirection(string direction);
     }
 }
--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserProfileRepository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserProfileRepository.cs
@@ -1,41 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using DIMS_Core.DataAccessLayer.Context;
-using DIMS_Core.DataAccessLayer.Interfaces;
+﻿using DIMS_Core.DataAccessLayer.Context;
 using DIMS_Core.DataAccessLayer.Entities;
-using System.Threading.Tasks;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using DIMS_Core.Common.Extensions;
+using DIMS_Core.DataAccessLayer.Interfaces;
 
 namespace DIMS_Core.DataAccessLayer.Repositories
 {
-    public class VUserProfileRepository:Repository<VUserProfile>, IVUserProfileRepository
+    public class VUserProfileRepository : Repository<VUserProfile>, IVUserProfileRepository
     {
-        public VUserProfileRepository(DIMSCoreDatabaseContext dbContext) : base(dbContext) { }
-
-        public async Task<VUserProfile> GetVUserProfileByEmail(string email)
+        public VUserProfileRepository(DIMSCoreDatabaseContext dbContext) : base(dbContext)
         {
-            if (email.IsNullOrWhiteSpace())
-            {
-                return null;
-            }
-            
-            var entity = await currentSet.FirstAsync(e => e.Email == email);
-            return entity;
-        }
-
-        public IQueryable<VUserProfile> GetVUserProfilesByDirection(string direction)
-        {
-            if (direction.IsNullOrWhiteSpace())
-            {
-                return null;
-            }
-
-            var query = GetAll();
-            query = query.Where(o=>o.Direction==direction);
-            return query;
         }
     }
 }
