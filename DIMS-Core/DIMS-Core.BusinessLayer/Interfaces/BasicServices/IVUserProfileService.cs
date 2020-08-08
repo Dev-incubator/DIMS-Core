2c3a73e432d57bbbb59af1354eb88aa8d6894266
darkpchela <darkpchela2@gmail.com>
2020-08-08 12:21:34 +0300

    unnecessary methods removed

--- DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/BasicServices/IVUserProfileService.cs
+++ DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/BasicServices/IVUserProfileService.cs
@@ -1,15 +1,8 @@
 ﻿using DIMS_Core.BusinessLayer.Models.BaseModels;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace DIMS_Core.BusinessLayer.Interfaces
 {
     public interface IVUserProfileService : IBasicCRUDService<VUserProfileModel>
     {
-        Task<VUserProfileModel> GetUserProfileViewById(int userId);
-
-        Task<VUserProfileModel> GetUserProfileViewByEmail(string email);
-
-        Task<IEnumerable<VUserProfileModel>> GetUserProfileViewsByDirection(string direction);
     }
 }
