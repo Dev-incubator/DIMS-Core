1bffbe37b68b7e15fb2fca0c7134761fbebf7867
darkpchela <darkpchela2@gmail.com>
2020-08-08 20:22:37 +0300

    Progress Views anf model updated

---
+++ DIMS-Core/DIMS-Core/Models/ProgressModels/CreateEditNoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIMS_Core.Models.ProgressModels
+{
+    public class CreateEditNoteViewModel
+    {
+        public int UserTaskId { get; set; }
+        public DateTime TrackDate { get; set; }
+        public string TrackNote { get; set; }
+        public string TaskName { get; set; }
+    }
+}
