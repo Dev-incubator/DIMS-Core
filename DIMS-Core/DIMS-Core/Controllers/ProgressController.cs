1c82153a6c95bf3be7c4324bba850d73d65051b7
darkpchela <darkpchela2@gmail.com>
2020-08-08 19:37:14 +0300

    Added carcase of MemberTask ManageGrid

--- DIMS-Core/DIMS-Core/Controllers/ProgressController.cs
+++ DIMS-Core/DIMS-Core/Controllers/ProgressController.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.Models.ProgressModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DIMS_Core.Controllers
 {
     public class ProgressController : Controller
     {
-        private IUserProfileService userProfileService { get; set; }
         private IVUserTaskService  vUserTaskService {get;set;}
         public ProgressController()
         {
 
         }
 
-        public IActionResult MembersTasksManageGrid(int UserId, string UserName)
+        public async Task<IActionResult> MembersTasksManageGrid(int UserId, string UserName)
         {
-            return View();
+            var model = new MembersTasksViewModel();
+            var allUserTask = await vUserTaskService.GetAllAsync();
+            model.UserName = UserName;
+            model.userTaskModels = allUserTask.Where(ut=>ut.UserId==UserId);
+
+            return View(model);
         }
 
         public IActionResult TaskTracksManageGrid()
@@ -26,11 +31,37 @@ namespace DIMS_Core.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> CreateNote(int TaskId)
+        {
+            return PartialView("CreateNoteWindow");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNote()
+        {
+            return PartialView("CreateNoteWindow");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditNote(int id)
+        {
+            return PartialView("EditNoteWindow");
+        }
+
+        [HttpPost]
         public async Task<IActionResult> EditNote()
         {
             return PartialView("EditNoteWindow");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> DeleteNote(int id)
+        {
+            return View("TaskTracksManageGrid");
+        }
+
+        [HttpPost]
         public async Task<IActionResult> DeleteNote()
         {
             return View("TaskTracksManageGrid");
---
+++ DIMS-Core/DIMS-Core/Models/ProgressModels/MembersTasksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DIMS_Core.BusinessLayer.Models.BaseModels;
+using System.Collections.Generic;
+
+namespace DIMS_Core.Models.ProgressModels
+{
+    public class MembersTasksViewModel
+    {
+        public string UserName { get; set; }
+        public IEnumerable<VUserTaskModel> userTaskModels { get; set; }
+    }
+}
