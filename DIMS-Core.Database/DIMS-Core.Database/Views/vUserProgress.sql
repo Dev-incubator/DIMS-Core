bd4a5cfcd1b2e69cabc25747803279c35fbb7d77
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-09-26 17:29:53 +0300

    Add view vUserTrack
    Update view vUserProgress

--- DIMS-Core.Database/DIMS-Core.Database/Views/vUserProgress.sql
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserProgress.sql
@@ -6,7 +6,7 @@
 		(UserProfile.[Name] + ' ' + UserProfile.LastName) as UserName,
 		Task.[Name] as TaskName,
 		TaskTrack.TrackNote,
-		TaskTrack.TrackDate
+		FORMAT(TaskTrack.TrackDate, 'dd/MM/yyyy hh:mm:ss') as TrackDate
 	FROM UserProfile
 		JOIN UserTask on UserProfile.UserId = UserTask.UserId
 		JOIN TaskTrack on UserTask.TaskId = TaskTrack.UserTaskId
---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTrack.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vUserTrack]
+	AS SELECT
+		UserTask.UserId,
+		UserTask.TaskId,
+		TaskTrack.TaskTrackId,
+		Task.[Name] as TaskName,
+		TaskTrack.TrackNote,
+		FORMAT(TaskTrack.TrackDate, 'dd/MM/yyyy hh:mm:ss') as TrackDate
+	FROM UserTask
+		JOIN Task on UserTask.TaskId = Task.TaskId
+		JOIN TaskTrack on UserTask.UserTaskId = TaskTrack.UserTaskId
