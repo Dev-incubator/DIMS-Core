757703a40717d7e77006902602f9332604ce91aa
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-09-26 16:53:36 +0300

    Add view vUserProfile
    Create folder Views

---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserProfile.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[vUserProfile] 
+	AS SELECT 
+		UserProfile.UserId,
+		(UserProfile.[Name] + ' ' + UserProfile.LastName) as FullName,
+		UserProfile.Email,
+		Direction.[Name] as Direction,
+		UserProfile.Sex,
+		UserProfile.Education,
+		(DATEDIFF(YEAR, UserProfile.BirthOfDate, GETDATE())) as Age,
+		UserProfile.UniversityAverageScore,
+		UserProfile.MathScore,
+		UserProfile.[Address],
+		UserProfile.MobilePhone,
+		UserProfile.Skype,
+		FORMAT(UserProfile.StartDate, 'dd/MM/yyyy', 'en-US') as UserProfile
+	FROM UserProfile INNER JOIN Direction ON UserProfile.DirectionId=Direction.DirectionId
