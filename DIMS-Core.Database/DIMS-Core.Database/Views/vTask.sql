31468af969f4817530f7400d14fb109f2c3d91fd
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-09-26 17:14:10 +0300

    Add view vTask

---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vTask.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[vTask]
+	AS SELECT 
+		Task.TaskId,
+		Task.[Name],
+		Task.[Description],
+		FORMAT(Task.StartDate, 'dd/MM/yyyy hh:mm') as StartDate,
+		FORMAT(Task.DeadlineDate, 'dd/MM/yyyy hh:mm') as DeadlineDate
+	FROM [Task]
