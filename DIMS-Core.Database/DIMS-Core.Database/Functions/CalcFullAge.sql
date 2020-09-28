6bd8f94da3398b30a589f84600405efa6e9a26b6
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-09-29 01:56:54 +0300

    Create SQL function CalcFullAge

---
+++ DIMS-Core.Database/DIMS-Core.Database/Functions/CalcFullAge.sql
@@ -0,0 +1,8 @@
+﻿CREATE FUNCTION [dbo].[CalcFullAge]
+(
+	@BirthOfDate Date
+)
+RETURNS int AS
+BEGIN
+	RETURN ((CONVERT(int,CONVERT(char(8),GETDATE(),112))-CONVERT(char(8),@BirthOfDate,112))/10000)
+END
