6356f6181b96432567d0fddb49a7dd7d91e9ff23
Konstantin <33716293+PushMeNow@users.noreply.github.com>
2020-09-30 23:26:58 +0300

    Remove all old logic

--- DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IUnitOfWork.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -6,17 +6,6 @@ namespace DIMS_Core.DataAccessLayer.Interfaces
     public interface IUnitOfWork : IDisposable
     {
         ISampleRepository SampleRepository { get; }
-        IDirectionRepository DirectionRepository { get; }
-        ITaskRepository TaskRepository { get; }
-        ITaskStateRepository TaskStateRepository { get; }
-        ITaskTrackRepository TaskTrackRepository { get; }
-        IUserProfileRepository UserProfileRepository { get; }
-        IUserTaskRepository UserTaskRepository { get; }
-        IVTaskRepository VTaskRepository { get; }
-        IVUserProfileRepository VUserProfileRepository { get; }
-        IVUserProgressRepository VUserProgressRepository { get; }
-        IVUserTaskRepository VUserTaskRepository { get; }
-        IVUserTrackRepository VUserTrackRepository { get; }
         Task SaveAsync();
     }
 }
