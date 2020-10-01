69d7eaa57cad7fc8e798c4de126884cc1f941c7a
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-01 15:29:57 +0300

    Create VUserProgressRepository, IVUserProgressRepository
    Interfaces/IVUserProgressRepository
    Repositories/VUserProgressRepository

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/IVUserProgressRepository.cs
@@ -0,0 +1,14 @@
+﻿using DIMS_Core.DataAccessLayer.Context;
+using DIMS_Core.DataAccessLayer.Entities;
+using DIMS_Core.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIMS_Core.DataAccessLayer.Repositories
+{
+    public class VUserProgressRepository : Repository<VUserProgress>, IVUserProgressRepository
+    {
+        public VUserProgressRepository(DIMSCoreDatabaseContext dbContext) : base(dbContext) { }
+    }
+}
