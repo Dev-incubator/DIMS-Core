93354a7fcca57009100c37b01cf0f74c42479fd0
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-01 15:08:15 +0300

    Create VUserProfileRepository, IVUserProfileRepository
    Interfaces/IVUserProfileRepository
    Repositories/VUserProfileRepository

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using DIMS_Core.DataAccessLayer.Context;
+using DIMS_Core.DataAccessLayer.Entities;
+using DIMS_Core.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIMS_Core.DataAccessLayer.Repositories
+{
+    public class VUserProfileRepository : Repository<VUserProfile>, IVUserProfileRepository
+    {
+        public VUserProfileRepository(DIMSCoreDatabaseContext dbContext) : base(dbContext) { }
+    }
+}
