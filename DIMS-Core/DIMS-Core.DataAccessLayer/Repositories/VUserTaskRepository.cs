3dde6c84aba2770b7b7138a1d117c52553f015d6
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-01 15:38:58 +0300

    Create VUserTaskRepository, IVUserTaskRepository
    Interfaces/IVUserTaskRepository
    Repositories/VUserTaskRepository

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserTaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using DIMS_Core.DataAccessLayer.Context;
+using DIMS_Core.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIMS_Core.DataAccessLayer.Repositories
+{
+    public class VUserTaskRepository : Repository<VUserTask>, IVUserTaskRepository
+    {
+        public VUserTaskRepository(DIMSCoreDatabaseContext dbContext) : base(dbContext) { }
+    }
+}
