6770d3ba9eaeb4e20ceaeb6ceee36dfbcfc26c24
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-02 16:49:36 +0300

    MemberController constructor was updated

--- DIMS-Core/DIMS-Core/Controllers/MemberController.cs
+++ DIMS-Core/DIMS-Core/Controllers/MemberController.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.BusinessLayer.Models.Samples;
+using DIMS_Core.Models.Sample;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DIMS_Core.Controllers
 {
-    public class MemberController
+    [Route("members")]
+    public class MemberController : Controller
     {
+        private readonly IMemberService memberService;
+        private readonly IMapper mapper;
+
+        public MemberController(IMemberService memberService, IMapper mapper)
+        {
+            this.memberService = memberService;
+            this.mapper = mapper;
+        }
     }
 }
