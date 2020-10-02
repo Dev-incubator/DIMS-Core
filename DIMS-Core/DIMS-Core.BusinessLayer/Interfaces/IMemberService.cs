67849d886b55b932dc654e7f66d58a23c133d2d1
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-02 16:34:34 +0300

    IMemberService was added

--- DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/IMemberService.cs
+++ DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/IMemberService.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using DIMS_Core.BusinessLayer.Models.Members;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DIMS_Core.BusinessLayer.Interfaces
 {
-    interface IMemberService
+    public interface IMemberService
     {
+        //Task<IEnumerable<MemberModel>> SearchAsync(SampleFilter searchFilter);
+
+        Task<MemberModel> GetMemberAsync(int id);
+
+        Task CreateAsync(MemberModel model);
+
+        Task DeleteAsync(int id);
+
+        Task UpdateAsync(MemberModel model);
     }
 }
