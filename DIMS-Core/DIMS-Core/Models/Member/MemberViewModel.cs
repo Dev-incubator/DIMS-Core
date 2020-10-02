a7cfcb027d647d421205779b076c49096770d469
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-02 14:47:41 +0300

    MemberViewModel created

---
+++ DIMS-Core/DIMS-Core/Models/Member/MemberViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIMS_Core.Models.Member
+{
+    public class MemberViewModel
+    {
+        public int MemberId { get; set; }
+
+        public string Direction { get; set; }
+
+        public string Education { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int Age { get; set; }
+    }
+}
