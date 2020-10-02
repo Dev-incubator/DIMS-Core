e69e8f2f1a06a746e2c9499fdeeae2baae5a787f
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-02 16:45:55 +0300

    MemberService was added

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Services/MemberService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.BusinessLayer.Models.Members;
+using DIMS_Core.DataAccessLayer.Filters;
+using DIMS_Core.DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIMS_Core.BusinessLayer.Services
+{
+    internal class MemberService : IMemberService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public MemberService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public Task<IEnumerable<MemberModel>> SearchAsync(MemberFilter searchFilter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemberModel> GetMemberAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(MemberModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(MemberModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
