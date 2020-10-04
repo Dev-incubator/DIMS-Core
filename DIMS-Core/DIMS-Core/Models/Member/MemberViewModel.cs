bb61fa2a638889956362b657a2d5fd4649530574
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-04 21:02:09 +0300

    MemberAddViewModel was created. MemberViewModel was modified. View memberCreate was created

--- DIMS-Core/DIMS-Core/Models/Member/MemberViewModel.cs
+++ DIMS-Core/DIMS-Core/Models/Member/MemberViewModel.cs
@@ -11,16 +11,9 @@ namespace DIMS_Core.Models.Member
     {
         public int UserId { get; set; }
         public string FullName { get; set; }
-        public string Email { get; set; }
         public string Direction { get; set; }
-        public Sex Sex { get; set; }
         public string Education { get; set; }
         public int? Age { get; set; }
-        public double? UniversityAverageScore { get; set; }
-        public double? MathScore { get; set; }
-        public string Address { get; set; }
-        public string MobilePhone { get; set; }
-        public string Skype { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime? StartDate { get; set; }
     }
