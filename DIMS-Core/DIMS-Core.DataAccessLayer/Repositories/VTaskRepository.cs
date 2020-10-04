a3684bcc1ed8eb3c27ec70582a5e45e11e9fd7a9
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-04 16:27:16 +0300

    Add Search() method to VTaskRepository

--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VTaskRepository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VTaskRepository.cs
@@ -3,6 +3,7 @@ using DIMS_Core.DataAccessLayer.Entities;
 using DIMS_Core.DataAccessLayer.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DIMS_Core.DataAccessLayer.Repositories
@@ -10,5 +11,10 @@ namespace DIMS_Core.DataAccessLayer.Repositories
     public class VTaskRepository : Repository<VTask>, IVTaskRepository
     {
         public VTaskRepository(DIMSCoreDatabaseContext dbContext) : base(dbContext) { }
+
+        public IQueryable<VTask> Search()
+        {
+            return GetAll();
+        }
     }
 }
