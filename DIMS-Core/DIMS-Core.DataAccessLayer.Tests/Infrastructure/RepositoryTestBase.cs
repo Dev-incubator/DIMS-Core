177b19249138a89ef126c0ea321264a12c802b0f
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-09 18:45:40 +0300

    Add Infrastructure for repositories tests

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer.Tests/Infrastructure/RepositoryTestBase.cs
@@ -0,0 +1,38 @@
+﻿using DIMS_Core.DataAccessLayer.Context;
+using DIMS_Core.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIMS_Core.DataAccessLayer.Tests.Infrastructure
+{
+    public class RepositoryTestBase : IDisposable
+    {
+        protected readonly DIMSCoreDatabaseContext context;
+
+        public RepositoryTestBase()
+        {
+            var options = new DbContextOptionsBuilder<DIMSCoreDatabaseContext>()
+                   .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                   .Options;
+
+            context = new DIMSCoreDatabaseContext(options);
+            context.Database.EnsureCreated();
+
+            Seed(context);
+        }
+
+        private void Seed(DIMSCoreDatabaseContext context)
+        {
+
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
