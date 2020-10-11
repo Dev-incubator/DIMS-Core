00d8dbaed1e6d44c56a11f61087152108096aad6
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-12 02:30:48 +0300

    Added validation attributes to editMemberViewModel

--- DIMS-Core/DIMS-Core/Models/Member/EditMemberViewModel.cs
+++ DIMS-Core/DIMS-Core/Models/Member/EditMemberViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using DIMS_Core.DataAccessLayer.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,9 @@ namespace DIMS_Core.Models.Member
     public class EditMemberViewModel
     {
         public int UserId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string Education { get; set; }
         public double? UniversityAverageScore { get; set; }
