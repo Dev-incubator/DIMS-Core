7faa356b81c6a7599f32c06c2d2e7dbe6ef460af
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-11 03:42:19 +0300

    Update seed data in RepositoryTestBase

--- DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Infrastructure/RepositoryTestBase.cs
+++ DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Infrastructure/RepositoryTestBase.cs
@@ -3,6 +3,8 @@ using DIMS_Core.DataAccessLayer.Entities;
 using DIMS_Core.DataAccessLayer.Enums;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Data;
+using System.Net.WebSockets;
 
 namespace DIMS_Core.Tests.Infrastructure
 {
@@ -24,62 +26,135 @@ namespace DIMS_Core.Tests.Infrastructure
 
         private void Seed(DIMSCoreDatabaseContext context)
         {
-            SeedVUserProfiles(context);
+            #region Seed Directions
+                var directions = new[]
+                   {
+                    new Direction {
+                        DirectionId = 1,
+                        Name = "-",
+                        Description = "I haven't decided, I need a consultation"
+                    },
+                    new Direction {
+                        DirectionId = 2,
+                        Name = "FRONTEND",
+                        Description = "Front-end description text"
+                    },
+                    new Direction {
+                        DirectionId = 3,
+                        Name = "JAVA",
+                        Description = "Java description text"
+                    },
+                    new Direction {
+                        DirectionId = 4,
+                        Name = ".NET",
+                        Description = ".Net description text"
+                    },
+                    new Direction {
+                        DirectionId = 5,
+                        Name = "SALESFORCE",
+                        Description = "Front-end description text"
+                    }
+                };
+
+                context.Direction.AddRange(directions);
+            #endregion
+
+            #region Seed UserProfiles
+                var userProfiles = new[]
+                {
+                    new UserProfile {
+                        UserId = 1,
+                        Name = "Elisey",
+                        LastName = "Butko",
+                        Email = "elisey.butko@gmail.com",
+                        DirectionId = 3,
+                        Direction = directions[2],
+                        Sex = Sex.Female,
+                        Education = "БГУИР",
+                        BirthOfDate = new DateTime(2000,08,20),
+                        UniversityAverageScore = 9.0,
+                        MathScore = 4.1,
+                        Address = "Минск, ул. Макаёнка 21, 32",
+                        MobilePhone = "375296002085",
+                        Skype = "elisey.butko",
+                        StartDate = new DateTime(2020,08,20)
+                    },
+                    new UserProfile {
+                        UserId = 2,
+                        Name = "Mark",
+                        LastName = "Davydov",
+                        Email = "mark.davydov@gmail.com",
+                        DirectionId = 2,
+                        Direction = directions[1],
+                        Sex = Sex.Female,
+                        Education = "БГУ",
+                        BirthOfDate = new DateTime(1995,08,20),
+                        UniversityAverageScore = 8.5,
+                        MathScore = 6.1,
+                        Address = "Минск, ул. Дачная 15",
+                        MobilePhone = "375291755175",
+                        Skype = "mark.davydov",
+                        StartDate = new DateTime(2020,09,01)
+                    },
+                    new UserProfile {
+                        UserId = 3,
+                        Name = "Peter",
+                        LastName = "Doronin",
+                        Email = "peter.doronin@gmail.com",
+                        DirectionId = 4,
+                        Direction = directions[3],
+                        Sex = Sex.Female,
+                        Education = "БНТУ",
+                        BirthOfDate = new DateTime(2000,08,20),
+                        UniversityAverageScore = 7.5,
+                        MathScore = 8.1,
+                        Address = "Минск, ул. Дружбы 5, 100",
+                        MobilePhone = "375445552096",
+                        Skype = "peter2000",
+                        StartDate = new DateTime(2020,07,30),
+                    },
+                };
+
+                context.UserProfile.AddRange(userProfiles);
+            #endregion
+
+            #region Seed VUserProfiles
+                var vUserProfiles = new VUserProfile[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    vUserProfiles[i] = new VUserProfile
+                    {
+                        UserId = userProfiles[i].UserId,
+                        FullName = userProfiles[i].Name + " " + userProfiles[i].LastName,
+                        Email = userProfiles[i].Email,
+                        Direction = userProfiles[i].Direction.Name,
+                        Sex = userProfiles[i].Sex,
+                        Education = userProfiles[i].Education,
+                        Age = GetFullAge(userProfiles[i].BirthOfDate),
+                        UniversityAverageScore = userProfiles[i].UniversityAverageScore,
+                        MathScore = userProfiles[i].MathScore,
+                        Address = userProfiles[i].Address,
+                        MobilePhone = userProfiles[i].MobilePhone,
+                        Skype = userProfiles[i].Skype,
+                        StartDate = userProfiles[i].StartDate
+                    };
+                };
+
+                context.VUserProfile.AddRange(vUserProfiles);
+            #endregion
+
             context.SaveChanges();
         }
 
-        private void SeedVUserProfiles(DIMSCoreDatabaseContext context)
+        private int? GetFullAge(DateTime? birthdate)
         {
-            var vUserProfiles = new[]
+            if (birthdate.HasValue)
             {
-                new VUserProfile {
-                    UserId = 1,
-                    FullName = "Elisey Butko",
-                    Email = "elisey.butko@gmail.com",
-                    Direction = "JAVA",
-                    Sex = Sex.Female,
-                    Education = "БГУИР",
-                    Age = 21,
-                    UniversityAverageScore = 9.0,
-                    MathScore = 4.1,
-                    Address = "Минск, ул. Макаёнка 21, 32",
-                    MobilePhone = "375296002085",
-                    Skype = "elisey.butko",
-                    StartDate = new System.DateTime(2020,08,20)
-                },
-                new VUserProfile {
-                    UserId = 2,
-                    FullName = "Mark Davydov",
-                    Email = "mark.davydov@gmail.com",
-                    Direction = "FRONTEND",
-                    Sex = Sex.Female,
-                    Education = "БГУ",
-                    Age = 25,
-                    UniversityAverageScore = 8.5,
-                    MathScore = 6.1,
-                    Address = "Минск, ул. Дачная 15",
-                    MobilePhone = "375291755175",
-                    Skype = "mark.davydov",
-                    StartDate = new System.DateTime(2020,09,01)
-                },
-                new VUserProfile {
-                    UserId = 3,
-                    FullName = "Peter Doronin",
-                    Email = "peter.doronin@gmail.com",
-                    Direction = ".NET",
-                    Sex = Sex.Female,
-                    Education = "БНТУ",
-                    Age = 20,
-                    UniversityAverageScore = 7.5,
-                    MathScore = 8.1,
-                    Address = "Минск, ул. Дружбы 5, 100",
-                    MobilePhone = "375445552096",
-                    Skype = "peter2000",
-                    StartDate = new System.DateTime(2020,07,30)
-                },
-            };
-
-            context.VUserProfile.AddRange(vUserProfiles);
+                var age = DateTime.Today.Year - birthdate.Value.Year;
+                if (birthdate > DateTime.Today.AddYears(-age)) age--;
+                return age;
+            }
+            else return null;
         }
 
         public void Dispose()
