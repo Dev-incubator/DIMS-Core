e7e724851069baad3a0b8f978aff093d72c717db
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-10-11 23:48:44 +0300

    Update SeedVUserTasks

--- DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Infrastructure/RepositoryTestBase.cs
+++ DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Infrastructure/RepositoryTestBase.cs
@@ -29,67 +29,124 @@ namespace DIMS_Core.Tests.Infrastructure
 
         private void Seed(DIMSCoreDatabaseContext context)
         {
-            SeedVUserProfiles(context);
-            SeedVUserTasks(context);
-            context.SaveChanges();
-        }
+            #region Seed Directions
+            var directions = new[]
+               {
+                    new Direction {
+                        DirectionId = 1,
+                        Name = "-",
+                        Description = "I haven't decided, I need a consultation"
+                    },
+                    new Direction {
+                        DirectionId = 2,
+                        Name = "FRONTEND",
+                        Description = "Front-end description text"
+                    },
+                    new Direction {
+                        DirectionId = 3,
+                        Name = "JAVA",
+                        Description = "Java description text"
+                    },
+                    new Direction {
+                        DirectionId = 4,
+                        Name = ".NET",
+                        Description = ".Net description text"
+                    },
+                    new Direction {
+                        DirectionId = 5,
+                        Name = "SALESFORCE",
+                        Description = "Front-end description text"
+                    }
+                };
 
-        private void SeedVUserProfiles(DIMSCoreDatabaseContext context)
-        {
-            var vUserProfiles = new[]
+            context.Direction.AddRange(directions);
+            #endregion
+
+            #region Seed UserProfiles
+            var userProfiles = new[]
             {
-                new VUserProfile {
-                    UserId = 1,
-                    FullName = "Elisey Butko",
-                    Email = "elisey.butko@gmail.com",
-                    Direction = "JAVA",
-                    Sex = Sex.Female,
-                    Education = "БГУИР",
-                    Age = 21,
-                    UniversityAverageScore = 9.0,
-                    MathScore = 4.1,
-                    Address = "Минск, ул. Макаёнка 21, 32",
-                    MobilePhone = "375296002085",
-                    Skype = "elisey.butko",
-                    StartDate = new System.DateTime(2020,08,20)
-                },
-                new VUserProfile {
-                    UserId = 2,
-                    FullName = "Mark Davydov",
-                    Email = "mark.davydov@gmail.com",
-                    Direction = "FRONTEND",
-                    Sex = Sex.Female,
-                    Education = "БГУ",
-                    Age = 25,
-                    UniversityAverageScore = 8.5,
-                    MathScore = 6.1,
-                    Address = "Минск, ул. Дачная 15",
-                    MobilePhone = "375291755175",
-                    Skype = "mark.davydov",
-                    StartDate = new System.DateTime(2020,09,01)
-                },
-                new VUserProfile {
-                    UserId = 3,
-                    FullName = "Peter Doronin",
-                    Email = "peter.doronin@gmail.com",
-                    Direction = ".NET",
-                    Sex = Sex.Female,
-                    Education = "БНТУ",
-                    Age = 20,
-                    UniversityAverageScore = 7.5,
-                    MathScore = 8.1,
-                    Address = "Минск, ул. Дружбы 5, 100",
-                    MobilePhone = "375445552096",
-                    Skype = "peter2000",
-                    StartDate = new System.DateTime(2020,07,30)
-                },
+                    new UserProfile {
+                        UserId = 1,
+                        Name = "Elisey",
+                        LastName = "Butko",
+                        Email = "elisey.butko@gmail.com",
+                        DirectionId = 3,
+                        Direction = directions[2],
+                        Sex = Sex.Female,
+                        Education = "БГУИР",
+                        BirthOfDate = new DateTime(2000,08,20),
+                        UniversityAverageScore = 9.0,
+                        MathScore = 4.1,
+                        Address = "Минск, ул. Макаёнка 21, 32",
+                        MobilePhone = "375296002085",
+                        Skype = "elisey.butko",
+                        StartDate = new DateTime(2020,08,20)
+                    },
+                    new UserProfile {
+                        UserId = 2,
+                        Name = "Mark",
+                        LastName = "Davydov",
+                        Email = "mark.davydov@gmail.com",
+                        DirectionId = 2,
+                        Direction = directions[1],
+                        Sex = Sex.Female,
+                        Education = "БГУ",
+                        BirthOfDate = new DateTime(1995,08,20),
+                        UniversityAverageScore = 8.5,
+                        MathScore = 6.1,
+                        Address = "Минск, ул. Дачная 15",
+                        MobilePhone = "375291755175",
+                        Skype = "mark.davydov",
+                        StartDate = new DateTime(2020,09,01)
+                    },
+                    new UserProfile {
+                        UserId = 3,
+                        Name = "Peter",
+                        LastName = "Doronin",
+                        Email = "peter.doronin@gmail.com",
+                        DirectionId = 4,
+                        Direction = directions[3],
+                        Sex = Sex.Female,
+                        Education = "БНТУ",
+                        BirthOfDate = new DateTime(2000,08,20),
+                        UniversityAverageScore = 7.5,
+                        MathScore = 8.1,
+                        Address = "Минск, ул. Дружбы 5, 100",
+                        MobilePhone = "375445552096",
+                        Skype = "peter2000",
+                        StartDate = new DateTime(2020,07,30),
+                    },
+                };
+
+            context.UserProfile.AddRange(userProfiles);
+            #endregion
+
+            #region Seed VUserProfiles
+            var vUserProfiles = new VUserProfile[3];
+            for (int i = 0; i < 3; i++)
+            {
+                vUserProfiles[i] = new VUserProfile
+                {
+                    UserId = userProfiles[i].UserId,
+                    FullName = userProfiles[i].Name + " " + userProfiles[i].LastName,
+                    Email = userProfiles[i].Email,
+                    Direction = userProfiles[i].Direction.Name,
+                    Sex = userProfiles[i].Sex,
+                    Education = userProfiles[i].Education,
+                    Age = GetFullAge(userProfiles[i].BirthOfDate),
+                    UniversityAverageScore = userProfiles[i].UniversityAverageScore,
+                    MathScore = userProfiles[i].MathScore,
+                    Address = userProfiles[i].Address,
+                    MobilePhone = userProfiles[i].MobilePhone,
+                    Skype = userProfiles[i].Skype,
+                    StartDate = userProfiles[i].StartDate
+                };
             };
 
             context.VUserProfile.AddRange(vUserProfiles);
-        }
+            #endregion
 
-        private void SeedVUserTasks(DIMSCoreDatabaseContext context)
-        {
+            #region SeedVUserTasks
             var vUserTasks = new[]
             {
                 new VUserTask {
@@ -101,9 +158,40 @@ namespace DIMS_Core.Tests.Infrastructure
                     DeadlineDate = new DateTime(2020, 12,04),
                     State = "In work"
                 },
+                new VUserTask {
+                    TaskId = 2,
+                    TaskName = "Write CRUD operations for Users",
+                    Description = "Write create, read, update and delete operations for Users",
+                    UserId = 1,
+                    StartDate = new DateTime(2020,07,30),
+                    DeadlineDate = new DateTime(2020, 12,04),
+                    State = "In work"
+                },
+                 new VUserTask {
+                    TaskId = 3,
+                    TaskName = "Write CRUD operations for Tasks",
+                    Description = "Write create, read, update and delete operations for Tasks",
+                    UserId = 2,
+                    StartDate = new DateTime(2020,07,30),
+                    DeadlineDate = new DateTime(2020, 12,04),
+                    State = "In work"
+                }
             };
 
             context.VUserTask.AddRange(vUserTasks);
+            #endregion
+            context.SaveChanges();
+        }
+
+        private int? GetFullAge(DateTime? birthdate)
+        {
+            if (birthdate.HasValue)
+            {
+                var age = DateTime.Today.Year - birthdate.Value.Year;
+                if (birthdate > DateTime.Today.AddYears(-age)) age--;
+                return age;
+            }
+            else return null;
         }
 
         [OneTimeTearDown]
--- DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Repositories/DirectionRepositoryTest.cs
+++ DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Repositories/DirectionRepositoryTest.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DIMS_Core.DataAccessLayer.Repositories;
+using DIMS_Core.Tests.Infrastructure;
+using NUnit.Framework;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using Xunit;
 
 namespace DIMS_Core.Tests.Repositories
 {
-    class DirectionRepositoryTest
+    public class DirectionRepositoryTest : RepositoryTestBase
     {
+        [Fact]
+        public void ShouldReturnAllSearch()
+        {
+            // Arrange
+            int countUsers = 1;
+            var query = new VTaskRepository(context);
+
+            // Act
+            var result = query.Search();
+
+            //Assert
+            Assert.Equals(countUsers, result.Count());
+        }
+
+        [Fact]
+        public void ShouldReturnAll()
+        {
+            // Arrange
+            int countUsers = 1;
+            var query = new VTaskRepository(context);
+
+            // Act
+            var result = query.GetAll();
+
+            //Assert
+            Assert.Equals(countUsers, result.Count());
+        }
+
+        [Fact]
+        public void ShouldReturnById()
+        {
+            // Arrange
+            int getId = 1;
+            string returnTaskName = "Create database";
+            var query = new VTaskRepository(context);
+
+            // Act
+            var result = query.GetByIdAsync(getId);
+
+            //Assert
+            Assert.Equals(returnTaskName, result.Result.Name);
+        }
     }
 }
