81bf1017a63ab354c0487264987d72fdeb12b90f
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-11 03:39:31 +0300

    Update test VUserProfileRepositoryTests

--- DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Repositories/VUserProfileRepositoryTests.cs
+++ DIMS-Core/DIMS-Core.Tests/DataAccessLayer/Repositories/VUserProfileRepositoryTests.cs
@@ -3,35 +3,52 @@ using DIMS_Core.Tests.Infrastructure;
 using System.Linq;
 using System.Linq.Dynamic.Core;
 using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace DIMS_Core.DataAccessLayer.Tests
 {
     public class VUserProfileRepositoryTests : RepositoryTestBase
     {
         [Test]
-        public void ShouldReturnAll()
+        public void ShouldReturnAllSearch()
         {
             // Arrange
+            int countUsers = 3;
             var query = new VUserProfileRepository(context);
 
             // Act
             var result = query.Search();
 
             //Assert
-            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(countUsers, result.Count());
         }
 
         [Test]
-        public void ShouldReturnById()
+        public void ShouldReturnAll()
         {
             // Arrange
+            int countUsers = 3;
             var query = new VUserProfileRepository(context);
 
             // Act
-            var result = query.GetByIdAsync(1);
+            var result = query.GetAll();
 
             //Assert
-            Assert.AreEqual("Elisey Butko", result.Result.FullName);
+            Assert.AreEqual(countUsers, result.Count());
+        }
+
+        [Test]
+        public async Task ShouldReturnById()
+        {
+            // Arrange
+            int getId = 1;
+            var query = new VUserProfileRepository(context);
+
+            // Act
+            var result = await query.GetByIdAsync(getId);
+
+            //Assert
+            Assert.AreEqual("Elisey Butko", result.FullName);
         }
     }
 }
