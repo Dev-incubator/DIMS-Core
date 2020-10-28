62339c5863691f335ae4042ffe1f6200b8d218d8
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-10-28 07:42:51 +0300

    Remove inheritance from the model (TaskWithMembersViewModel)

--- DIMS-Core/DIMS-Core/Models/Task/TaskWithMembersViewModel.cs
+++ DIMS-Core/DIMS-Core/Models/Task/TaskWithMembersViewModel.cs
@@ -1,15 +1,32 @@
-﻿using DIMS_Core.DataAccessLayer.Enums;
-using DIMS_Core.Models.Member;
+﻿using DIMS_Core.BusinessLayer.Models.Task;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DIMS_Core.Models.Task
 {
-    public class TaskWithMembersViewModel : TaskViewModel
+    public class TaskWithMembersViewModel
     {
-        public List<MemberForTaskViewModel> Members { get; set; }
+        public int TaskId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Deadline date is required")]
+        public DateTime DeadlineDate { get; set; }
+
+        public List<MemberForTaskModel> Members { get; set; } = new List<MemberForTaskModel>();
+
+        public TaskWithMembersViewModel() { }
+
+        public TaskWithMembersViewModel(List <MemberForTaskModel> Members)
+        {
+            StartDate = DateTime.Now;
+            DeadlineDate = DateTime.Now;
+            this.Members = Members;
+        }
     }
 }
