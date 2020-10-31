e05be0c7601351462fdcb9221387890cc8a0dd3e
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-10-31 18:17:42 +0300

    Add TaskTracks models and mapping

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Models/TaskTracks/VUserTrackModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIMS_Core.BusinessLayer.Models.TaskTracks
+{
+    public class VUserTrackModel
+    {
+        public int UserId { get; set; }
+        public int TaskId { get; set; }
+        public int TaskTrackId { get; set; }
+        public string TaskName { get; set; }
+        public string TrackNote { get; set; }
+        public DateTime TrackDate { get; set; }
+    }
+}
