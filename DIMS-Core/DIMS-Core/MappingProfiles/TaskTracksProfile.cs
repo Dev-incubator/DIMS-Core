b0d4d5c9b182215b57811dfce60be66b2f299199
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-10-31 18:17:42 +0300

    Add TaskTracks models and mapping
    (cherry picked from commit e05be0c7601351462fdcb9221387890cc8a0dd3e)

---
+++ DIMS-Core/DIMS-Core/MappingProfiles/TaskTracksProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DIMS_Core.BusinessLayer.Models.TaskTracks;
+using DIMS_Core.Models.TaskTracks;
+
+namespace DIMS_Core.MappingProfiles
+{
+    public class TaskTracksProfile : Profile
+    {
+        public TaskTracksProfile()
+        {
+            CreateMap<VUserTrackModel, TaskTracksViewModel>().ReverseMap();
+        }
+    }
+}
