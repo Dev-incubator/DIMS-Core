14337028aef028f0ffb54ad6e55cb69c7dfa771b
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-10-31 18:16:34 +0300

    Add TaskTrack service

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Interfaces/ITaskTracksService.cs
@@ -0,0 +1,11 @@
+﻿using DIMS_Core.BusinessLayer.Models.TaskTracks;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DIMS_Core.BusinessLayer.Interfaces
+{
+    public interface ITaskTracksService
+    {
+        Task<IEnumerable<VUserTrackModel>> GetAll();
+    }
+}
