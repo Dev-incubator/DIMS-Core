7797f8be77f5570345a8f0ba79c842b928359e60
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-10-31 18:16:34 +0300

    Add TaskTrack service
    (cherry picked from commit 14337028aef028f0ffb54ad6e55cb69c7dfa771b)

---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Services/TaskTracksService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.DataAccessLayer.Interfaces;
+using DIMS_Core.BusinessLayer.Models.TaskTracks;
+
+namespace DIMS_Core.BusinessLayer.Services
+{
+    internal class TaskTracksService : ITaskTracksService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public TaskTracksService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<VUserTrackModel>> GetAll()
+        {
+            var taskTracks = unitOfWork.VUserTrackRepository.GetAll();
+            var mappedQuery = mapper.ProjectTo<VUserTrackModel>(taskTracks);
+
+            return await mappedQuery.ToListAsync();
+        }
+    }
+}
