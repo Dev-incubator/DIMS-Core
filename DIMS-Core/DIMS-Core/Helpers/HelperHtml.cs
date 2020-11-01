61aa4d74a7c7a34d4c321c963567dd5118aa0099
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-01 22:28:20 +0300

    Add method CreateMembersSimpleList to HelperHtml

--- DIMS-Core/DIMS-Core/Helpers/HelperHtml.cs
+++ DIMS-Core/DIMS-Core/Helpers/HelperHtml.cs
@@ -10,22 +10,46 @@ namespace DIMS_Core.Helpers
         public static HtmlString CreateMembersList(this IHtmlHelper html, List<MemberForTaskModel> members)
         {
             string result = "<div class=\"form-control checkbox-list\">\n";
-            for (int i = 0; i < members.Count; i++)
-            { 
-                result += 
-                    "<div class=\"item\">"
-                        + $"<input type=\"hidden\" name=\"Members[{i}].UserTaskId\" value=\"{members[i].UserTaskId}\">"
-                        + $"<input type=\"hidden\" name=\"Members[{i}].UserId\" value=\"{members[i].UserId}\">"
-                        + $"<input type=\"hidden\" name=\"Members[{i}].FullName\" value=\"{members[i].FullName}\">"
-                        + "<label>"
-                            + $"<input type=\"checkbox\" name=\"Members[{i}].Selected\" value=\"true\"" 
-                            + $"{(members[i].Selected ? "checked= \"checked\"" : null)}>"
-                            + $"<span>{members[i].FullName}</span>"
-                        + "</label>"
-                    + "</div>";
+            if (members is null || members.Count == 0)
+            {
+
+            }
+            else
+            {
+                for (int i = 0; i < members.Count; i++)
+                {
+                    result +=
+                        "<div class=\"item\">"
+                            + $"<input type=\"hidden\" name=\"Members[{i}].UserTaskId\" value=\"{members[i].UserTaskId}\">"
+                            + $"<input type=\"hidden\" name=\"Members[{i}].UserId\" value=\"{members[i].UserId}\">"
+                            + $"<input type=\"hidden\" name=\"Members[{i}].FullName\" value=\"{members[i].FullName}\">"
+                            + "<label>"
+                                + $"<input type=\"checkbox\" name=\"Members[{i}].Selected\" value=\"true\""
+                                + $"{(members[i].Selected ? "checked= \"checked\"" : null)}>"
+                                + $"<span>{members[i].FullName}</span>"
+                            + "</label>"
+                        + "</div>";
+                }
             }
             result += "</div>";
             return new HtmlString(result);
+        }
+
+        public static HtmlString CreateMembersSimpleList(this IHtmlHelper html, List<MemberForTaskModel> members)
+        {
+            if (members is null || members.Count == 0)
+            {
+                return new HtmlString("<div>-</div>");
+            }
+            else
+            {
+                string result = string.Empty;
+                foreach (var member in members)
+                {
+                    result += $"<div>{member.FullName}</div>";
+                }
+                return new HtmlString(result);
+            }
         }
     }
 }
