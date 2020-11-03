7f5b7571f148b406ab17ea9b22d57770b39198da
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-03 03:01:00 +0300

    Create TaskTrackService & UserTaskService

--- DIMS-Core/DIMS-Core.BusinessLayer/Extensions/MiddlewareServiceExtensions.cs
+++ DIMS-Core/DIMS-Core.BusinessLayer/Extensions/MiddlewareServiceExtensions.cs
@@ -19,7 +19,8 @@ namespace DIMS_Core.BusinessLayer.Extensions
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<IMemberService, MemberService>();
             services.AddTransient<ITaskService, TaskService>();
-            services.AddTransient<ITaskTracksService, TaskTracksService>();
+            services.AddTransient<ITaskTrackService, TaskTrackService>();
+            services.AddTransient<IUserTaskService, UserTaskService>();
 
             services.AddDatabaseDependencies()
                 .AddIndentityDependencies()
---
+++ DIMS-Core/DIMS-Core.BusinessLayer/Services/UserTaskService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DIMS_Core.BusinessLayer.Interfaces;
+using DIMS_Core.DataAccessLayer.Interfaces;
+using DIMS_Core.BusinessLayer.Models.UserTask;
+using System.Linq;
+
+namespace DIMS_Core.BusinessLayer.Services
+{
+    internal class UserTaskService : IUserTaskService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public UserTaskService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<UserTaskModel>> GetAllForMember(int userId)
+        {
+            var query = unitOfWork.UserTaskRepository.GetAll()
+                .Where(userTask => userTask.UserId == userId);
+
+            var mappedQuery = mapper.ProjectTo<UserTaskModel>(query);
+
+            return await mappedQuery.ToListAsync();
+        }
+    }
+}
