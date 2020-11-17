30d9d39ba5fafc8de8485d61a03462a8099c77e6
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-17 23:14:49 +0300

    Refactoring in TaskTrackController

--- DIMS-Core/DIMS-Core/Controllers/TaskTrackController.cs
+++ DIMS-Core/DIMS-Core/Controllers/TaskTrackController.cs
@@ -37,11 +37,6 @@ namespace DIMS_Core.Controllers
         {
             var currentUser = await memberService.GetMemberByEmail(User.Identity.Name);
 
-            if (currentUser is null)
-            {
-                return RedirectToAction("Index", "Home", new { });
-            }
-
             var taskTracks = await taskTrackService.GetAllByUserId(currentUser.UserId);
             var model = mapper.Map<IEnumerable<VTaskTrackViewModel>>(taskTracks);
 
@@ -66,25 +61,15 @@ namespace DIMS_Core.Controllers
         [HttpGet("create")]
         public async Task<IActionResult> Create(int userTaskId = 0, string back = null, string backAction = null)
         {
-            if (!User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("Index", "Home", new { });
-            }
-
-            var currentUser = await memberService.GetMemberByEmail(User.Identity.Name);
-
-            if (currentUser is null)
-            {
-                return RedirectToAction("Index", "Home", new { });
-            }
-
             var model = new TaskTrackViewModel
             {
                 UserTaskId = userTaskId,
                 TrackDate = DateTime.Now
             };
 
+            var currentUser = await memberService.GetMemberByEmail(User.Identity.Name);
             var userTasks = await userTaskService.GetAllByUserId(currentUser.UserId);
+
             ViewBag.SelectListUserTasks = new SelectList(userTasks, "UserTaskId", "Task.Name");
             ViewBag.BackController = back;
             ViewBag.BackAction = backAction;
@@ -115,13 +100,12 @@ namespace DIMS_Core.Controllers
                 return BadRequest();
             }
 
-            // To Do - Get the id of the current user
-            int userId = 3;
-
             var taskTrack = await taskTrackService.GetTaskTrack(id);
             var model = mapper.Map<TaskTrackViewModel>(taskTrack);
 
-            var userTasks = await userTaskService.GetAllByUserId(userId);
+            var currentUser = await memberService.GetMemberByEmail(User.Identity.Name);
+            var userTasks = await userTaskService.GetAllByUserId(currentUser.UserId);
+
             ViewBag.SelectListUserTasks = new SelectList(userTasks, "UserTaskId", "Task.Name");
             ViewBag.BackController = back;
             return View(model);
@@ -131,19 +115,18 @@ namespace DIMS_Core.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit([FromForm] TaskTrackViewModel model)
         {
-            // To Do - Get the id of the current user
-            int userId = 3;
+            var currentUser = await memberService.GetMemberByEmail(User.Identity.Name);
 
             if (!ModelState.IsValid)
             {
-                var userTasks = await userTaskService.GetAllByUserId(userId);
+                var userTasks = await userTaskService.GetAllByUserId(currentUser.UserId);
                 ViewBag.SelectListUserTasks = new SelectList(userTasks, "UserTaskId", "Task.Name");
                 return View(model);
             }
 
             if (model.TaskTrackId <= 0)
             {
-                var userTasks = await userTaskService.GetAllByUserId(userId);
+                var userTasks = await userTaskService.GetAllByUserId(currentUser.UserId);
                 ViewBag.SelectListUserTasks = new SelectList(userTasks, "UserTaskId", "Task.Name");
                 return View(model);
             }
