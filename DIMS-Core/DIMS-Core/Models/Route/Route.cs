3e89ee4a63f01f2a10ddf96e89750e1e4508c9df
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-29 17:19:39 +0300

    Add class Route to build the path

---
+++ DIMS-Core/DIMS-Core/Models/Route/Route.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DIMS_Core.Models.Route
+{
+    public class Route
+    {
+        [BindingBehavior(BindingBehavior.Optional)]
+        public string Controller { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public string Action { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public string BackController { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public string BackAction { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public int? TaskId { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public int? UserId { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public int? UserTaskId { get; set; }
+        [BindingBehavior(BindingBehavior.Optional)]
+        public int? TaskTrackId { get; set; }
+    }
+}
