a5fca8a1f384919c2f2d5cab6bf6dc43d027f99d
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-29 17:21:23 +0300

    Update class BottomButtonsViewModel

--- DIMS-Core/DIMS-Core/Models/Route/BottomButtonsViewModel.cs
+++ DIMS-Core/DIMS-Core/Models/Route/BottomButtonsViewModel.cs
@@ -1,11 +1,12 @@
-﻿namespace DIMS_Core.Models
+﻿namespace DIMS_Core.Models.Route
 {
     public class BottomButtonsViewModel
     {
-        public int Id { get; set; }
+        public IdWithName Id { get; set; }
+        public string Action { get; set; }
         public string Controller { get; set; }
+        public IdWithName BackId { get; set; }
         public string BackAction { get; set; }
         public string BackController { get; set; }
-        public int UserId { get; set; }
     }
 }
