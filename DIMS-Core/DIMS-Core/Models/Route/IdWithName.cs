d0333dc3f46bd949e594402bcadf03cfd3a2d539
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-29 17:22:24 +0300

    Create class IdWithName to build the path

---
+++ DIMS-Core/DIMS-Core/Models/Route/IdWithName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIMS_Core.Models.Route
+{
+    public class IdWithName
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+
+        public IdWithName(int id, string name)
+        {
+            Value = id;
+            Name = name;
+        }
+    }
+}
