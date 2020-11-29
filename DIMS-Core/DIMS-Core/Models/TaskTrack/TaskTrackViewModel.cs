cee1ba68e12e40cde36e0d97cf3c163f8b9b8078
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-29 17:37:17 +0300

    Add validation for TaskTrack

--- DIMS-Core/DIMS-Core/Models/TaskTrack/TaskTrackViewModel.cs
+++ DIMS-Core/DIMS-Core/Models/TaskTrack/TaskTrackViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DIMS_Core.Models.TaskTrack
 {
@@ -6,7 +7,9 @@ namespace DIMS_Core.Models.TaskTrack
     {
         public int TaskTrackId { get; set; }
         public int UserTaskId { get; set; }
+        [Required(ErrorMessage = "Track date is required")]
         public DateTime TrackDate { get; set; }
+        [Required(ErrorMessage = "Track note is required")]
         public string TrackNote { get; set; }
     }
 }
