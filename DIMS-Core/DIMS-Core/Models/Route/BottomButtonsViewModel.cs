6daffbe29f9fc8d98a7555c177aa37b00a2d7d35
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-12-01 17:31:52 +0300

    Manual cherry pick from #29

---
+++ DIMS-Core/DIMS-Core/Models/Route/BottomButtonsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DIMS_Core.Models.Route
+{
+    public class BottomButtonsViewModel
+    {
+        public IdWithName Id { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public IdWithName BackId { get; set; }
+        public string BackAction { get; set; }
+        public string BackController { get; set; }
+    }
+}
---
+++ DIMS-Core/DIMS-Core/Models/Route/IdWithName.cs
@@ -0,0 +1,14 @@
+﻿namespace DIMS_Core.Models.Route
+{
+    public class IdWithName
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+
+        public IdWithName(int id, string name)
+        {
+            Value = id;
+            Name = name;
+        }
+    }
+}
